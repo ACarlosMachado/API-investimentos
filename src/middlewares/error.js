57906d81bcb57297a3ba6922ed0814adce84e857
ACarlosMachado <antoniocarlos_acrm@hotmail.com>
2022-07-20 23:27:07 -0300

    Refactor: midd erro

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,7 +1,12 @@
 const Joi = require('joi');
 
 const errorHandler = (err, _req, res, _next) => {
-    if (Joi.isError(err)) return res.status(400).json({ message: err.message });
+    if (Joi.isError(err) && err.message.includes('required')) {
+        return res.status(400).json({ message: err.message });
+    }
+    if (Joi.isError(err)) {
+        return res.status(422).json({ message: err.message });
+    }
     return res.status(500).json({ err });
 };
 
